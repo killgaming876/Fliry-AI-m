6a8896a96d84037a2a60e00f111a90d35d133c8a
BloxFruitsScripters <131740181+killgaming876@users.noreply.github.com>
2026-05-28 12:04:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FlirtyAI from "./FliryAI";
+import FlirtyAI from "./FlirtyAI_fixed";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
